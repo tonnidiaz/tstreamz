ef5588f677ddb91f531eb04acee2631a5426c723
tonnidiaz <therealhackingknight@gmail.com>
2025-02-15 15:53:06 +0200

    with cors

--- apps/tu-metatags/src/middleware.ts
+++ apps/tu-metatags/src/middleware.ts
@@ -1,16 +1,36 @@
 import { tuErr } from "@repo/ui-next/lib/funcs";
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
-export const middleware = async (req: NextRequest)=>{
+export const middleware = async (req: NextRequest) => {
     console.log("\n[MIDDLEWARE]");
-    console.log(req.url);
-    const user = req.nextUrl.searchParams.get('user')
-    // const res = NextResponse.next()
-    if (user != 'tonni'){
-         return tuErr("UNAUTHORIZED", 401)
-        
-    }
-}
+    const res = NextResponse.next();
+    res.headers.set("Access-Control-Allow-Origin", "*");
+    res.headers.set(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.headers.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+    return res;
+};
 export const config = {
-    matcher: ['/admin/:path*']
-}
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        {
+            source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+            missing: [
+                { type: "header", key: "next-router-prefetch" },
+                { type: "header", key: "purpose", value: "prefetch" },
+            ],
+        },
+    ],
+};
